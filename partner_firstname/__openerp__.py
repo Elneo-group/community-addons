02e865b64f93acbb5db74a332848f5ffebfeed26
root <root@odoo>
2019-01-17 14:07:00 +0100

    Commit production

--- partner_firstname/__openerp__.py
+++ partner_firstname/__openerp__.py
@@ -21,13 +21,18 @@
 {
     'name': 'Partner first name and last name',
     'summary': "Split first name and last name for non company partners",
-    'version': '2.0',
-    'author': "Camptocamp,Odoo Community Association (OCA)",
+    'version': '8.0.2.2.1',
+    "author": "Camptocamp, "
+              "Grupo ESOC Ingeniería de Servicios, "
+              "ACSONE SA/NV, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
     'maintainer': 'Camptocamp, Acsone',
     'category': 'Extra Tools',
-    'website': 'http://www.camptocamp.com, http://www.acsone.eu',
-    'depends': ['base'],
+    'website': 'https://odoo-community.org',
+    'depends': ['base_setup'],
     'data': [
+        'views/base_config_view.xml',
         'views/res_partner.xml',
         'views/res_user.xml',
         'data/res_partner.yml',
--- partner_firstname/tests/test_empty.py
+++ partner_firstname/tests/test_empty.py
@@ -1,5 +1,4 @@
-# -*- encoding: utf-8 -*-
-
+# -*- coding: utf-8 -*-
 # Odoo, Open Source Management Solution
 # Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
 #
@@ -23,7 +22,7 @@ To have more accurate results, remove the ``mail`` module before testing.
 
 from openerp.tests.common import TransactionCase
 from .base import MailInstalled
-from .. import exceptions as ex
+from ..models import exceptions as ex
 
 
 class CompanyCase(TransactionCase):
