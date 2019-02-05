6476bbaf884b86602eb4f11396db1811b16bf148
Grifnée Loïc <loic.grifnee@elneo.com>
2019-02-05 13:40:50 +0100

    account_move_line_search_extension upgrade

--- account_move_line_search_extension/static/src/js/account_move_line_search_extension.js
+++ account_move_line_search_extension/static/src/js/account_move_line_search_extension.js
@@ -1,3 +1,7 @@
+/*
+# Copyright 2009-2016 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+*/
 openerp.account_move_line_search_extension = function (instance) {
     var QWeb = instance.web.qweb;
 
@@ -7,7 +11,6 @@ openerp.account_move_line_search_extension = function (instance) {
     instance.account_move_line_search_extension.ListSearchView = instance.web.ListView.extend({
 
         init: function() {
-            var self = this;
             this._super.apply(this, arguments);
             this.journals = [];
             this.current_account = null;
@@ -15,16 +18,22 @@ openerp.account_move_line_search_extension = function (instance) {
             this.current_partner = null;
             this.current_journal = null;
             this.current_period = null;
+            this.current_reconcile = null;
             this.options.addable = false;
-            this.set_user_groups();
+            this.set_render_dict();
         },
 
         start: function(){
             var tmp = this._super.apply(this, arguments);
             var self = this;
-            this.$el.parent().prepend(QWeb.render('AccountMoveLineSearchExtension', self.groups_dict));
+            this.$el.parent().prepend(QWeb.render('AccountMoveLineSearchExtension', self.render_dict));
             self.set_change_events();
             return tmp;
+        },
+
+        is_action_enabled: function(action) {
+            /* remove 'Delete' from Sidebar */
+            return action=='delete' ? false : this._super.apply(this, arguments);
         },
 
         set_change_events: function() {
@@ -49,21 +58,18 @@ openerp.account_move_line_search_extension = function (instance) {
                     self.current_period = this.value === '' ? null : this.value;
                     self.do_search(self.last_domain, self.last_context, self.last_group_by);
                 });
+            this.$el.parent().find('.oe_account_select_reconcile').change(function() {
+                    self.current_reconcile = this.value === '' ? null : this.value;
+                    self.do_search(self.last_domain, self.last_context, self.last_group_by);
+                });
         },
 
-        set_user_groups: function() {
-            var self = this;
-            var result = {};
-            var action_context = this.dataset.get_context().__contexts[1];
-            _.each(action_context, function(v,k) {
-                if (k[v] && (k.slice(0, 6) === "group_")) {
-                    result[k] = true;
-                }
-                else {
-                    result[k] = false;
-                };
-            });
-            self.groups_dict = result;
+        set_render_dict: function() {
+            /*
+            Customise this function to modify the rendering dict for the qweb template.
+            By default the action context is passed as rendering dict.
+            */
+            this.render_dict = this.dataset.get_context().__contexts[1];
         },
 
         do_search: function(domain, context, group_by) {
@@ -98,6 +104,7 @@ openerp.account_move_line_search_extension = function (instance) {
             if (self.current_partner) domain.push(['partner_id.name', 'ilike', self.current_partner]);
             if (self.current_journal) domain.push(['journal_id', '=', self.current_journal]);
             if (self.current_period) domain.push('|',['period_id.code', 'ilike', self.current_period],['period_id.name', 'ilike', self.current_period]);
+            if (self.current_reconcile) domain.push(['reconcile_ref', '=ilike', self.current_reconcile]);
             //_.each(domain, function(x) {console.log('amlse, aml_search_domain, domain_part = ', x)});
             return domain;
         },
