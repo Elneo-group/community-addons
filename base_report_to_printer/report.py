c2d093b278c8f4b56ddb37c7222eca29792e6d8d
loicgrifnee <loic.grifnee@elneo.com>
2019-03-28 11:18:29 +0100

    no report to printer when neopost

--- base_report_to_printer/report.py
+++ base_report_to_printer/report.py
@@ -82,7 +82,7 @@ class Report(models.Model):
         can_print_report = self._can_print_report(cr, uid, ids,
                                                   behaviour, printer, document,
                                                   context=context)
-        if can_print_report:
+        if can_print_report and ("force_neopost_send" in context and not context['force_neopost_send']) or "force_neopost_send" not in context:
             printer.print_document(report, document, report.report_type)
         return document
 
