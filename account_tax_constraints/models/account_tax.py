0b84ea64574f849afb899a729571476b0c17cf97
Luc De Meyer <luc.demeyer@noviat.com>
2020-08-24 15:04:54 +0200

    [13.0]add account_tax_constraints

---
+++ account_tax_constraints/models/account_tax.py
@@ -0,0 +1,32 @@
+# Copyright 2009-2020 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, models
+from odoo.exceptions import UserError
+
+
+class AccountTax(models.Model):
+    _inherit = "account.tax"
+
+    def unlink(self):
+        for tax in self:
+            products = (
+                self.env["product.template"]
+                .with_context(active_test=False)
+                .search(
+                    ["|", ("supplier_taxes_id", "=", tax.id), ("taxes_id", "=", tax.id)]
+                )
+            )
+            if products:
+                product_list = ["%s" % x.name for x in products]
+                raise UserError(
+                    _(
+                        "You cannot delete a tax that "
+                        "has been set on product records"
+                        "\nAs an alterative, you can disable a "
+                        "tax via the 'active' flag."
+                        "\n\nProduct records: %s"
+                    )
+                    % product_list
+                )
+        return super().unlink()
