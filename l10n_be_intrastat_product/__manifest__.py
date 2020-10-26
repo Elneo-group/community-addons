05968a1c9bd11d2fdcea5385359422a3852647c4
Luc De Meyer <luc.demeyer@noviat.com>
2020-10-26 09:11:40 +0100

    [13.0][ADD]intrastat suite

---
+++ l10n_be_intrastat_product/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2009-2020 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Intrastat Product Declaration for Belgium",
+    "version": "13.0.1.1.0",
+    "category": "Intrastat",
+    "license": "AGPL-3",
+    "summary": "Intrastat Product Declaration for Belgium",
+    "author": "Noviat",
+    "depends": ["base_vat_sanitized", "intrastat_product"],
+    "conflicts": ["l10n_be_intrastat", "report_intrastat"],
+    "data": [
+        "security/intrastat_security.xml",
+        "security/ir.model.access.csv",
+        "data/intrastat_region.xml",
+        "data/intrastat_transaction.xml",
+        "views/account_move_views.xml",
+        "views/intrastat_installer_views.xml",
+        "views/l10n_be_intrastat_product_views.xml",
+    ],
+    "installable": True,
+}
