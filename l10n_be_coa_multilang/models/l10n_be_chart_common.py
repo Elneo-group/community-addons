799b9893e4a9785643752377b7abbb1760acc16f
Luc De Meyer <luc.demeyer@noviat.com>
2021-01-22 19:43:13 +0100

    [13.0]latest l10n_be_coa_multilang

--- l10n_be_coa_multilang/models/l10n_be_chart_common.py
+++ l10n_be_coa_multilang/models/l10n_be_chart_common.py
@@ -18,7 +18,7 @@ class L10nBeChartCommon(models.AbstractModel):
     color = fields.Char(help="CSS color unit")
     font = fields.Selection(selection=lambda self: self._selection_font())
     sequence = fields.Integer(default=0)
-    level = fields.Integer(string="Level",store=True,compute="_compute_level",)
+    level = fields.Integer(compute="_compute_level", string="Level", store=True)
     info = fields.Text()
 
     @api.model
