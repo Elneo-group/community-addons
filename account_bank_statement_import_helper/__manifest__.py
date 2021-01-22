2623f568be67d4c3f201f34bafda6ec938ca50f1
Luc De Meyer <luc.demeyer@noviat.com>
2021-01-22 19:54:49 +0100

    [13.0]latest-coda-and-prereqs

--- account_bank_statement_import_helper/__manifest__.py
+++ account_bank_statement_import_helper/__manifest__.py
@@ -1,15 +1,12 @@
-# Copyright 2009-2020 Noviat.
+# Copyright 2009-2021 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Bank statement import helper",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Accounting & Finance",
-    "summary": """
-        Generic bank statement import improvements.
-    """,
-    "author": "Noviat, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/bank-statement-import",
+    "author": "Noviat",
+    "website": "www.noviat.com",
     "depends": ["account_bank_statement_import"],
     "installable": True,
     "license": "AGPL-3",
