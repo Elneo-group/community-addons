b2cf9c4ffaa9eb369a813bc161cade3af80b1d68
Didderen Jérémy <jeremy.didderen@elneo.com>
2021-04-16 12:28:47 +0200

    barcode generator update

--- barcodes_generator_abstract/models/barcode_generate_mixin.py
+++ barcodes_generator_abstract/models/barcode_generate_mixin.py
@@ -6,42 +6,40 @@
 
 import logging
 
+import barcode
+
 from odoo import _, api, exceptions, fields, models
 
 from .barcode_rule import _GENERATE_TYPE
 
 _logger = logging.getLogger(__name__)
 
-try:
-    import barcode
-except ImportError:
-    _logger.debug("Cannot import 'python-barcode' python library.")
-    barcode = None
-
 
 class BarcodeGenerateMixin(models.AbstractModel):
-    _name = 'barcode.generate.mixin'
-    _description = 'Generate Barcode Mixin'
+    _name = "barcode.generate.mixin"
+    _description = "Generate Barcode Mixin"
 
     # Column Section
     barcode_rule_id = fields.Many2one(
-        string='Barcode Rule', comodel_name='barcode.rule')
+        string="Barcode Rule", comodel_name="barcode.rule"
+    )
 
-    barcode_base = fields.Integer(string='Barcode Base', copy=False)
+    barcode_base = fields.Integer(string="Barcode Base", copy=False)
 
     generate_type = fields.Selection(
-        string='Generate Type', selection=_GENERATE_TYPE, readonly=True,
-        related='barcode_rule_id.generate_type')
+        string="Generate Type",
+        selection=_GENERATE_TYPE,
+        readonly=True,
+        related="barcode_rule_id.generate_type",
+    )
 
     @api.model
     def create(self, vals):
         """It creates a new barcode if automation is active."""
-        barcode_rule = self.env['barcode.rule'].get_automatic_rule(self._name)
+        barcode_rule = self.env["barcode.rule"].get_automatic_rule(self._name)
         if barcode_rule.exists():
-            vals.update({
-                'barcode_rule_id': barcode_rule.id,
-            })
-        record = super(BarcodeGenerateMixin, self).create(vals)
+            vals.update({"barcode_rule_id": barcode_rule.id})
+        record = super().create(vals)
         if barcode_rule:
             record.generate_base()
             record.generate_barcode()
@@ -50,23 +48,29 @@ class BarcodeGenerateMixin(models.AbstractModel):
     # View Section
     def generate_base(self):
         for item in self:
-            if item.generate_type != 'sequence':
-                raise exceptions.UserError(_(
-                    "Generate Base can be used only with barcode rule with"
-                    " 'Generate Type' set to 'Base managed by Sequence'"))
+            if item.generate_type != "sequence":
+                raise exceptions.UserError(
+                    _(
+                        "Generate Base can be used only with barcode rule with"
+                        " 'Generate Type' set to 'Base managed by Sequence'"
+                    )
+                )
             else:
-                item.barcode_base =\
-                    item.barcode_rule_id.sequence_id.next_by_id()
+                item.barcode_base = item.barcode_rule_id.sequence_id.next_by_id()
 
     def generate_barcode(self):
         for item in self:
             padding = item.barcode_rule_id.padding
-            str_base = str(item.barcode_base).rjust(padding, '0')
+            str_base = str(item.barcode_base).rjust(padding, "0")
             custom_code = self._get_custom_barcode(item)
             if custom_code:
-                custom_code = custom_code.replace('.' * padding, str_base)
-                barcode_class = barcode.get_barcode_class(
-                    item.barcode_rule_id.encoding)
+                _logger.warning("custom code")
+                custom_code = custom_code.replace("." * padding, str_base)
+                _logger.warning(custom_code)
+                _logger.warning(item.barcode_rule_id.encoding)
+                barcode_class = barcode.get_barcode_class(item.barcode_rule_id.encoding)
+                _logger.warning(barcode_class)
+                _logger.warning(barcode_class(custom_code))
                 item.barcode = barcode_class(custom_code)
 
     # Custom Section
@@ -83,11 +87,9 @@ class BarcodeGenerateMixin(models.AbstractModel):
 
         # Define barcode
         custom_code = item.barcode_rule_id.pattern
-        custom_code = custom_code.replace('{', '').replace('}', '')
-        custom_code = custom_code.replace(
-            'D', self._get_replacement_char('D'))
-        return custom_code.replace(
-            'N', self._get_replacement_char('N'))
+        custom_code = custom_code.replace("{", "").replace("}", "")
+        custom_code = custom_code.replace("D", self._get_replacement_char("D"))
+        return custom_code.replace("N", self._get_replacement_char("N"))
 
     @api.model
     def _get_replacement_char(self, char):
@@ -96,4 +98,4 @@ class BarcodeGenerateMixin(models.AbstractModel):
         Define wich character will be used instead of the 'N' or the 'D'
         char, present in the pattern of the barcode_rule_id
         """
-        return '0'
+        return "0"
