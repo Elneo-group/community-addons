c0f27f10389c99d5142f445d926efaac278459f8
loicgrifnee <loic.grifnee@elneo.com>
2024-08-05 11:26:39 +0200

    account_move_misc_entry_no_automatic_tax

---
+++ account_move_misc_entry_no_automatic_tax/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2009-2023 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Misc Entries - No automatic tax",
+    "summary": "Do not set tax object when creating a miscellaneous accounting entry",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Noviat",
+    "website": "https://www.noviat.com/",
+    "category": "Accounting & Finance",
+    "depends": ["account"],
+    "installable": True,
+}
