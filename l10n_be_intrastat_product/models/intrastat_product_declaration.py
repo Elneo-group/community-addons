c4854c6c441ef83b65533b53e676a6690b869a9c
loicgrifnee <loic.grifnee@elneo.com>
2024-10-18 11:06:46 +0200

    fix

--- l10n_be_intrastat_product/models/intrastat_product_declaration.py
+++ l10n_be_intrastat_product/models/intrastat_product_declaration.py
@@ -269,7 +269,7 @@ class IntrastatProductDeclaration(models.Model):
         if self.declaration_type == "dispatches":
             etree.SubElement(
                 Item, "Dim", attrib={"prop": "EXCNTORI"}
-            ).text = line.product_origin_country_code or ""
+            ).text = line.product_origin_country_code or "QU"
             etree.SubElement(Item, "Dim", attrib={"prop": "PARTNERID"}).text = (
                 line.vat or ""
             )
